b57b5b68fbdf39b27eba7ac05846655899c31458
Totti26 <93136994+Totti26@users.noreply.github.com>
2022-03-10 08:49:52 -0600

    Add files via upload

---
+++ conversion_program.py
@@ -0,0 +1,23 @@
+def conversion_options():
+    print('1) Mi to Km')
+    print('2) Km to Mi')
+
+def mi_to_km():
+    mi = float(input('Enter mi: '))
+    km = mi * 1.60934
+    print(f'{mi} mi = {round(km,2)}km')
+
+def km_to_mi():
+    km = float(input('Enter km: '))
+    mi = km / 1.60934
+    print(f'{km} km: {round(mi,2)}mi')
+
+    
+conversion_options()
+user_choice = input('Which conversion would you like to do(enter #)? ')
+
+
+if user_choice =='1':
+    mi_to_km()
+if user_choice =='2':
+    km_to_mi()
